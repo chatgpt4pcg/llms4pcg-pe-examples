b977ea3891df349b3c919db4c494dd0076fa4d9a
Hachi <82988679+pichayakorn@users.noreply.github.com>
2024-12-29 15:45:37 +0900

    style(*): template formatting

--- few_shot/few_shot.py
+++ few_shot/few_shot.py
@@ -4,8 +4,6 @@ from llm4pcg.competition import chat_with_llm, run_evaluation
 from llm4pcg.models.trial_context import TrialContext
 from llm4pcg.models.trial_loop import TrialLoop
 
-
-
 class FewShotPrompting(TrialLoop):
     @staticmethod
     def run(ctx: TrialContext, target_character: str) -> str:
@@ -21,12 +19,14 @@ class FewShotPrompting(TrialLoop):
         response = response[0]
         return response
 
-
 if __name__ == "__main__":
-    #model_name = 'llama3'
     model_name = '<MODEL_NAME>'
     # local_model_base_url = e.g., 'http://localhost:1313/v1', 'http://localhost:11434/v1'
-    local_model_base_url =  '<LOCAL_MODEL_BASE_URL>'
-
-    run_evaluation("few_shot", FewShotPrompting, num_trials=1, characters=["A"],
-                   model_name=model_name, local_model_base_url=local_model_base_url)
+    local_model_base_url = '<LOCAL_MODEL_BASE_URL>'
+    # team_name = '<TEAM_NAME>'
+    team_name = 'few_shot'
+    run_evaluation(team_name=team_name, fn=FewShotPrompting, 
+                    model_name=model_name,
+                    local_model_base_url=local_model_base_url,
+                    num_trials=1, 
+                    characters=["A", "B", "C"])
